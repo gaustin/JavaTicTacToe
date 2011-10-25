b22f4c46a1f882ed088c4344b9fbcc4730253d45
Grant Austin <gaustin@gmail.com>
2011-10-25 12:46:40 -0600

    Converted tabs to spaces for Minimax*

--- src/tictactoe/players/Minimax.java
+++ src/tictactoe/players/Minimax.java
@@ -7,63 +7,65 @@ import tictactoe.game.TicTacToeScorer;
 
 public class Minimax {
 
-	private static int INFINITY = Integer.MAX_VALUE;
-	
-	public static int startMinimax(Board board, Player player) {
-		TicTacToeScorer scorer = new TicTacToeScorer(board);
-		
-		List<Integer> potentialMoves = board.emptySpaces();
-		
-		int bestMove = -1;
-		int bestMoveScore = -INFINITY;
-		
-		for (int move : potentialMoves) {
-			
-			board.markPosition(player.getMark(), move);
-			int score = minimax(board, scorer, player, 32);
-			board.markPosition('\0', move);
+    private static int INFINITY = Integer.MAX_VALUE;
 
-			if (bestMoveScore < score) {
-				bestMove = move;
-				bestMoveScore = score;
-			}
-			scorer.reset();
-		}
-		
-		return bestMove;
-	}
-	
-    private static int minimax(Board board, TicTacToeScorer scorer, Player player, int depth)
-    {
-    	Player opposingPlayer = player.getOpponent();
-        if(scorer.isGameOver() || depth <= 0) {
-			if (scorer.isDraw()) {
-				return 0;
-			} else if (scorer.getWinner() == player.getMark()) {
-				return INFINITY;
-			} else if (scorer.getWinner() == opposingPlayer.getMark()){
-				return -INFINITY;
-			} else {
-				return 0;
-			}
+    public static int startMinimax(Board board, Player player) {
+        TicTacToeScorer scorer = new TicTacToeScorer(board);
+
+        List<Integer> potentialMoves = board.emptySpaces();
+
+        int bestMove = -1;
+        int bestMoveScore = -INFINITY;
+
+        for (int move : potentialMoves) {
+
+            board.markPosition(player.getMark(), move);
+            int score = minimax(board, scorer, player, 32);
+            board.markPosition('\0', move);
+
+            if (bestMoveScore < score) {
+                bestMove = move;
+                bestMoveScore = score;
+            }
+            scorer.reset();
+        }
+
+        return bestMove;
+    }
+
+    private static int minimax(Board board, TicTacToeScorer scorer,
+            Player player, int depth) {
+        Player opposingPlayer = player.getOpponent();
+        if (scorer.isGameOver() || depth <= 0) {
+            if (scorer.isDraw()) {
+                return 0;
+            } else if (scorer.getWinner() == player.getMark()) {
+                return INFINITY;
+            } else if (scorer.getWinner() == opposingPlayer.getMark()) {
+                return -INFINITY;
+            } else {
+                return 0;
+            }
         } else {
-			List<Integer> potentialMoves = board.emptySpaces();
-			
-	        int bestPlayerScore = -INFINITY;
-			int bestOpponentScore = -INFINITY;
-			
-			for(int move : potentialMoves) {				
-				board.markPosition(opposingPlayer.getMark(), move);
-				bestOpponentScore = Math.max(minimax(board, scorer, opposingPlayer, depth - 1), bestOpponentScore);
-				board.markPosition('\0', move);
-			    
-				bestPlayerScore = -bestOpponentScore;
-				scorer.reset();
-			}
-			
-			// Deeper moves are less "minimal"
-			return bestPlayerScore > 0 ? bestPlayerScore - depth : bestPlayerScore + depth;
+            List<Integer> potentialMoves = board.emptySpaces();
+
+            int bestPlayerScore = -INFINITY;
+            int bestOpponentScore = -INFINITY;
+
+            for (int move : potentialMoves) {
+                board.markPosition(opposingPlayer.getMark(), move);
+                bestOpponentScore = Math.max(minimax(board, scorer,
+                        opposingPlayer, depth - 1), bestOpponentScore);
+                board.markPosition('\0', move);
+
+                bestPlayerScore = -bestOpponentScore;
+                scorer.reset();
+            }
+
+            // Deeper moves are less "minimal"
+            return bestPlayerScore > 0 ? bestPlayerScore - depth
+                    : bestPlayerScore + depth;
         }
     }
-	
+
 }
--- src/tictactoe/players/MinimaxPlayer.java
+++ src/tictactoe/players/MinimaxPlayer.java
@@ -3,13 +3,13 @@ package tictactoe.players;
 import tictactoe.game.Board;
 
 public class MinimaxPlayer extends Player {
-	
-	public MinimaxPlayer(char mark) {
-		super(mark);
-	}
-	
-	@Override
-	public int getChoice(Board board) {
-		return Minimax.startMinimax(board.copy(), this);
-	}
+    
+    public MinimaxPlayer(char mark) {
+        super(mark);
+    }
+    
+    @Override
+    public int getChoice(Board board) {
+        return Minimax.startMinimax(board.copy(), this);
+    }
 }
